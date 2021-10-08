a051e9865a96ae65fbbd1f72cc3d8e0a3af1605c
Necris45 <necris01@mail.ru>
2021-10-08 21:50:05 +0500

    устранил конфликт ветки с main

--- users/forms.py
+++ users/forms.py
@@ -52,3 +52,9 @@ class UserProfileForm(UserChangeForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
         self.fields['image'].widget.attrs['class'] = 'custom-file-input'
+
+    # def clean_image(self):
+    #     data = self.cleaned_data['image']
+    #     if data.size > 102400000:
+    #         raise forms.ValidationError('Файл слишком большой')
+    #     return data
