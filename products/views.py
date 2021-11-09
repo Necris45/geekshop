e0368bbb5d7ec97e6dc5656e1ad31af31cacf2e2
Necris45 <necris01@mail.ru>
2021-11-09 16:02:50 +0500

    used LOW_CACHE in products app

--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import ProductCategory, Product
 from django.views.generic import ListView, DetailView
 from django.urls import reverse_lazy
-
+from django.conf import settings
+from django.core.cache import cache
 
 # Create your views here.
 
@@ -15,6 +16,42 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
+def get_links_category():
+    if settings.LOW_CACHE:
+        key = 'links_category'
+        link_category = cache.get(key)
+        if link_category is None:
+            link_category = ProductCategory.objects.filter(is_active=True)
+            cache.set(key, link_category)
+        return link_category
+    else:
+        return ProductCategory.objects.filter(is_active=True)
+
+
+def get_link_product():
+    if settings.LOW_CACHE:
+        key = 'links_product'
+        link_product = cache.get(key)
+        if link_product is None:
+            link_product = Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+            cache.set(key, link_product)
+        return link_product
+    else:
+        return Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+
+
+def get_product(pk):
+    if settings.LOW_CACHE:
+        key = f'product{pk}'
+        product = cache.get(key)
+        if product is None:
+            product = get_object_or_404(Product, pk=pk)
+            cache.set(key, product)
+        return product
+    else:
+        return get_object_or_404(Product, pk=pk)
+
+
 class ProductListView(ListView):
     model = Product
     template_name = 'products/products.html'
@@ -24,7 +61,7 @@ class ProductListView(ListView):
     def get_context_data(self, *args, **kwargs):
         context = super(ProductListView, self).get_context_data(*args, **kwargs)
         context['title'] = 'Каталог'
-        context['categories'] = ProductCategory.objects.filter(is_active=True)
+        context['categories'] = get_links_category()
 
         category_id = None
         page_id = 1
@@ -35,9 +72,11 @@ class ProductListView(ListView):
         if 'page_id' in self.kwargs:
             page_id = self.kwargs['page_id']
 
-        goods = Product.objects.filter(category_id=category_id, is_active=True, category__is_active=True).\
-            select_related('category') if category_id is not None else \
-            Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+        # goods = Product.objects.filter(category_id=category_id, is_active=True, category__is_active=True).\
+        #     select_related('category') if category_id is not None else \
+        #     Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+
+        goods = get_link_product()
 
         paginator = Paginator(goods, per_page=3)
 
