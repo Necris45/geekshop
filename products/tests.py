8ae62a4dec5617b61653260612650b95e164f25e
Necris45 <necris01@mail.ru>
2021-11-12 19:23:05 +0500

    first test case (fixed)

--- products/tests.py
+++ products/tests.py
@@ -18,7 +18,7 @@ class TestMainSmokeTest(TestCase):
 
     #2 выполнения теста
     def test_products_pages(self):
-        response = self.client.get('/products/page/1/')
+        response = self.client.get('/products/detail/1/')
         print(response.status_code)
         # self.assertEqual(response.status_code, 200)
     #
